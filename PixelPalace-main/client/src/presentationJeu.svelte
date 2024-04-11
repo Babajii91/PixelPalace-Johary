545e289ae32997e2e543a53400879af18b3aeb4d
Babajii91 <jrakotonirina91@gmail.com>
2024-04-12 00:49:46 +0200

    modified:   PixelPalace-main/client/src/global.css 	modified:   PixelPalace-main/client/src/presentationGoodies.svelte 	modified:   PixelPalace-main/client/src/presentationJeu.svelte

---
+++ PixelPalace-main/client/src/presentationJeu.svelte
@@ -0,0 +1,110 @@
+<script>
+    let jeux = [
+        {
+            "nom": "Luigi's Mansion 2 - Nintendo Switch",
+            "image":"https://static.fnac-static.com/multimedia/Images/FR/MDM/e1/95/20/18912737/1540-1/tsp20240330092316/Luigi-s-Mansion-2-HD-Nintendo-Switch.jpg",
+            "prix": "49",
+            "type": "adventure",
+            "trailer": "https://youtu.be/fgh6-vMuPLI?si=WT_Qo6Q5BQBhKbCn",
+            "studio": "Nintendo",
+            "description":"Luigi's mansion 2 | HD | Nintendo switch"
+        },
+        {
+		"nom": "Super Mario Bros.™ Wonder Nintendo Switch",
+		"image":"https://static.fnac-static.com/multimedia/Images/FR/MDM/de/96/2d/19764958/1540-1/tsp20240301173710/Super-Mario-Bros-Wonder-Nintendo-Switch.jpg",
+		"prix": "49.99",
+		"type": "adventure",
+		"trailer": "https://youtu.be/1hcsH0ZyhwI?si=vbR-xCBCeEpjJNAP",
+		"studio": "Nintendo",
+		"description":"Attendez-vous à l'inattendu !Préparez-vous à de nombreuses surprises avec Super Mario Bros. Wonder, le tout nouveau Super Mario Bros en 2D exclusivement sur Nintendo Switch !"
+	},	
+	{
+		"nom": "The Last of Us™ Part I PS5",
+		"image":"https://static.fnac-static.com/multimedia/Images/FR/NR/2a/d8/cd/13490218/1505-1/tsp20240323151023/The-Last-of-Us-Part-I-PS5.jpg",
+		"prix": "79,99",
+		"type": "action",
+		"trailer": "https://youtu.be/_6v_D3QocIs?si=81U7uUiUjmTvxxc9",
+		"studio": "Naughty_Dog",
+		"description":"Dans un monde dévasté où rôdent infectés et survivants endurcis, Joel, un protagoniste désabusé, est engagé pour faire sortir Ellie, 14ans, d'une zone de quarantaine militaire. Mais ce qui devait être une simple mission de routine se transforme vite en un dangereux périple à travers le pays."
+	},	
+	{
+		"nom": "The Last Of Us Part II Remastered PS5",
+		"image":"https://static.fnac-static.com/multimedia/Images/FR/MDM/e0/83/20/18908128/3756-1/tsp20240306105414/The-Last-Of-Us-Part-II-Remastered-PS5.jpg",
+		"prix": "49.99",
+		"type": "action",
+		"trailer": "https://youtu.be/Ye3st9z6jQY?si=-eYdKq5UA15Li_pG",
+		"studio": "Naughty_Dog",
+		"description":"Cinq ans après leur voyage périlleux à travers une Amérique ravagée par une pandémie, Ellie et Joel ont trouvé refuge à Jackson, dans le Wyoming. Vivre parmi une communauté prospère de survivants leur a permis de trouver paix et stabilité, malgré la menace constante des infectés ainsi que des autres survivants. Lorsqu'un violent évènement vient rompre cette paix, Ellie se lance dans une quête de justice pour enfin tourner la page. Alors qu'elle pourchasse les responsables un par un, elle est confrontée aux répercussions dévastatrices de ses actions aussi bien physiques qu’émotionnelles."
+	},	
+	{
+		"nom": "God of War",
+		"image":"https://static.fnac-static.com/multimedia/FR/Images_Produits/FR/fnac.com/Visual_Principal_340/4/6/8/0711719635864/tsp20230620152433/God-Of-War.jpg",
+		"prix": "37",
+		"type": "action",
+		"trailer": "https://youtu.be/jZkX3LJDeYQ?si=jHtndC1A7YBHRJQ2",
+		"studio": "SIE_Santa_Monica_Studio ",
+		"description":"L'histoire se concentre autour de Kratos, un guerrier de Sparte qui livra sa vie à Arès lors d'une bataille contre les Barbares. Au service de celui-ci, Kratos tua sa femme et sa fille et commença à haïr Arès plus que tout. Il se mit au service des Dieux de l'Olympe pendant dix ans, ces derniers lui ayant promis de le soulager de ses visions. Un jour, Arès attaqua la ville d'Athènes et, à la demande d'Athéna, Kratos entama une quête pour tuer Arès."
+	},	
+	{
+		"nom": "God of War 2",
+		"image":"https://static.fnac-static.com/multimedia/FR/Images_Produits/FR/fnac.com/Visual_Principal_340/0/2/1/0711719904120/tsp20221209183926/God-of-War-2.jpg",
+		"prix": "13.17",
+		"type": "action",
+		"trailer": "https://youtu.be/x9YH8oGdcm8?si=Wdyq9JIPQb2MxkFq",
+		"studio": "SIE_Santa_Monica_Studio",
+		"description":"God of War II se déroule peu après les événements de God of War: Ghost of Sparta; Kratos, après avoir vaincu Ares, est devenu le nouveau dieu de la guerre, mais n'a malheureusement pas été accepté par les autres dieux en raison de son traitement impitoyable des autres cités grecques."
+	},	
+	{
+		"nom": "God of War 3",
+		"image":"https://static.fnac-static.com/multimedia/Images/FR/NR/0a/3f/ab/11222794/1540-1/tsp20240301002721/God-of-War-3-Hits-PS4.jpg",
+		"prix": "19.90",
+		"type": "action",
+		"trailer": "https://youtu.be/IWGD14e2M74?si=9TnzgUDh-93gL1hG",
+		"studio": "SIE_Santa_Monica_Studio ",
+		"description":"À la fin, seul règnera le chaos Devenez Kratos, l'intrépide guerrier spartiate, alors qu’il déchaîne sa rage sur les dieux qui l'ont trahi. Émergez des profondeurs du royaume d'Hadès et montez jusqu'au sommet du mont Olympe pour étancher la soif de vengeance de Kratos. Cet ultime chapitre de la trilogie God of War® est sans conteste un des plus grands spectacles vidéoludiques de l'histoire de PlayStation®. C'est parfait pour ceux qui souhaitent le revivre en 1080p pour une expérience encore plus éblouissante, ou pour ceux qui l'auraient raté la première fois. Enfin, avec le tout nouveau mode photo, vous pouvez interrompre les scènes d'action les plus ahurissantes, afin de modifier, enregistrer et partager des photos au cœur du carnage."
+	},
+	{
+		"nom": "God of War ragnarök",
+		"image":"https://static.fnac-static.com/multimedia/Images/FR/MDM/84/36/1f/18822788/1540-1/tsp20240301024547/God-of-War-Ragnarok-Edition-Standard-PS4.jpg",
+		"prix": "68",
+		"type": "adventure",
+		"trailer": "https://youtu.be/0taDnGk3oK4?si=l0AXpOHacZeVLZwi",
+		"studio": "SIE_Santa_Monica_Studio ",
+		"description":"God of War Ragnarok est un jeu d'action et d'aventure développé par le studio SIE Santa Monica. Il reprend l'histoire de Kratos et d'Atreus qui traversent les Neuf Royaumes à la recherche de réponses tandis que les forces asgardiennes se préparent à Ragnarok, une bataille prophétisée qui mettra fin au monde. Les joueurs exploreront des paysages mythiques et affronteront des dieux et des monstres nordiques. Il est disponible sur PlayStation 4 et PlayStation 5."
+	},	
+	{
+		"nom": "League Of Legends",
+		"image":"https://www.pedagojeux.fr/wp-content/uploads/2019/11/1280x720_LoL.jpg",
+		"prix": "Gratuit",
+		"type": "MOBA",
+		"trailer": "https://youtu.be/3VTkBuxU4yk?si=6B1vKax2Aw3Es0WG",
+		"studio": "RIOT_Games",
+		"description":"League of Legends est un jeu d'arène de combat en ligne multijoueurs (MOBA) dans lequel le joueur contrôle un personnage champion doté d'un ensemble de capacités uniques dans une perspective isométrique"
+	},	
+	{
+		"nom": "VALORANT",
+		"image":"https://image.jeuxvideo.com/medias/158341/1583411902-8477-jaquette-avant.jpg",
+		"prix": "Gratuit",
+		"type": "FPS",
+		"trailer": "https://youtu.be/RKAzrZWJQXA?si=9xtQ4qudPtymWEZc",
+		"studio": "RIOT_Games",
+		"description":"Valorant est un jeu vidéo multijoueur en ligne, produit par Riot Games. Il s'agit d'un jeu de tir à la première personne, composé de deux équipes de cinq personnes, où une équipe attaque et l'autre défend."
+	}
+];
+
+
+{#each jeux as jeu}
+    <div class="container">
+        <div class="video-container">
+            <iframe src={jeu.trailer} frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="game-info">
+            <img src={jeu.image} alt={jeu.nom} />
+            <h2>{jeu.nom}</h2>
+            <p>Prix: {jeu.prix}</p>
+            <p>Type: {jeu.type}</p>
+            <p>Studio: {jeu.studio}</p>
+            <p>Description: {jeu.description}</p>
+        </div>
+    </div>
+{/each}
